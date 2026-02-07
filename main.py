206c76ed333bc1186e7c1123af10300f0144dc40
andlit0vv <andlit0vv@gmail.com>
2026-02-07 03:28:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ app = Flask(__name__, template_folder='.')
   
 @app.route("/")  
 def web():  
-    return render_template('frontend/index.html')  
+    return render_template('frontend/index.html')  #New route
   
 if __name__ == "__main__":  
     app.run(debug=True, host="0.0.0.0", port=80)
+
 
